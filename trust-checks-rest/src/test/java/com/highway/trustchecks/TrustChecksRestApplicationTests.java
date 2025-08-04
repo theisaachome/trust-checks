880e9ac2d16e1ee685aeb11629699596af037b85
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-08-05 00:37:32 +0800

    restructure the project

---
+++ trust-checks-rest/src/test/java/com/highway/trustchecks/TrustChecksRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highway.trustchecks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustChecksRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
