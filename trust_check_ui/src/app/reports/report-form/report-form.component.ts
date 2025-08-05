63037a5562c0e3b924537bf85c75ae58c16346f9
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-08-06 02:17:53 +0800

    implemented report form

--- trust_check_ui/src/app/reports/report-form/report-form.component.ts
+++ trust_check_ui/src/app/reports/report-form/report-form.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-report-form',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './report-form.component.html',
   styleUrl: './report-form.component.css'
 })
 export class ReportFormComponent {
 
+  fileName: string ='';
+
+  onFileSelected (event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+        const file = input.files[0];
+        this.fileName = file.name;
+      console.log('selected file:' + file)
+    }
+  }
 }
